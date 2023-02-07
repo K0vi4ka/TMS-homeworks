11fd2238fd103db75a3c9e25300b9676c0b3b5c0
Zamiralov Ivan <zamiralovivan5@gmail.com>
2023-02-07 21:21:53 +0300

    Init project

---
+++ src/by/tms/homeworks/hw/hw_10022023/Program.java
@@ -0,0 +1,14 @@
+package by.tms.homeworks.hw.hw_10022023;
+
+public class Program {
+    public static void main(String[] args) {
+        int firstNum = 5;
+        int secondNum = 2;
+        int result = firstNum + secondNum;
+
+        System.out.println("Сумма чисел = "+ result);
+    }
+
+
+
+}

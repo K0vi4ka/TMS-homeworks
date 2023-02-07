6427bafa75bc2f1665c985b06a6ddc761afd02a1
Zamiralov Ivan <zamiralovivan5@gmail.com>
2023-02-07 21:23:12 +0300

    change sign

--- src/by/tms/homeworks/hw/hw_10022023/Program.java
+++ src/by/tms/homeworks/hw/hw_10022023/Program.java
@@ -4,9 +4,9 @@ public class Program {
     public static void main(String[] args) {
         int firstNum = 5;
         int secondNum = 2;
-        int result = firstNum + secondNum;
+        int result = firstNum - secondNum;
 
-        System.out.println("Сумма чисел = "+ result);
+        System.out.println("Разность чисел = "+ result);
     }
 
 

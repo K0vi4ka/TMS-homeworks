73472d3ae8c8f1aa46e1243f34628efa9ed36a7c
Zamiralov Ivan <zamiralovivan5@gmail.com>
2023-02-18 19:48:51 +0300

    fix bug hw_17 packege

--- src/by/tms/homeworks/hw/hw_17022023/ScanAndIf.java
+++ src/by/tms/homeworks/hw/hw_17022023/ScanAndIf.java
@@ -1,4 +1,4 @@
-package by.tms.homeworks.hw.hw_17022023.scannerAndIf;
+package by.tms.homeworks.hw.hw_17022023;
 
 import java.util.Scanner;
 
@@ -6,7 +6,7 @@ public class ScanAndIf {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int len,height,radius;
+        int len, height, radius;
         System.out.println("Введите следующие переменные");
         System.out.print("Длинна прямоугольника = ");
         len = in.nextInt();
@@ -15,10 +15,9 @@ public class ScanAndIf {
         System.out.print("Радиус круга = ");
         radius = in.nextInt();
 
-        if(Math.sqrt(Math.pow(len,2) + Math.pow(height,2)) < radius * 2) {
+        if (Math.sqrt(Math.pow(len, 2) + Math.pow(height, 2)) < radius * 2) {
             System.out.println("Круг полностью перекроет прямоугольник");
-        }
-        else {
+        } else {
             System.out.println("Круг не перекрывает прямоугольник");
         }
 
--- src/by/tms/homeworks/hw/hw_17022023/TwoDimArr.java
+++ src/by/tms/homeworks/hw/hw_17022023/TwoDimArr.java
@@ -1,4 +1,4 @@
-package by.tms.homeworks.hw.hw_17022023.twoDimArr;
+package by.tms.homeworks.hw.hw_17022023;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -13,27 +13,26 @@ public class TwoDimArr {
         int[][] arr = new int[n][n];
 
         System.out.println("Исходный массив");
-        for(int i = 0; i < n;i++) {
-            for (int j = 0;j< n;j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = rand.nextInt(10);
                 System.out.print(arr[i][j]);
             }
             System.out.println();
         }
-        for(int i = 0; i < n;i++) {
-            for (int j = 0; j < n;j++) {
-                if(arr[i][j] % 2 == 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] % 2 == 0) {
                     arr[i][j] = 0;
-                }
-                else{
+                } else {
                     arr[i][j] = 1;
                 }
             }
         }
 
         System.out.println("Полученный массив массив");
-        for(int i = 0; i < n;i++) {
-            for (int j = 0;j< n;j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();

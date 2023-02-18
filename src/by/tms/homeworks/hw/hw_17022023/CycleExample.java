efca2130dc6b230c648a50c5697964e7e5175c79
Zamiralov Ivan <zamiralovivan5@gmail.com>
2023-02-18 20:35:56 +0300

    fix bugs in hw_17022023 package

---
+++ src/by/tms/homeworks/hw/hw_17022023/CycleExample.java
@@ -0,0 +1,9 @@
+package by.tms.homeworks.hw.hw_17022023;
+
+public class CycleExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("*****");
+        }
+    }
+}
